945114b1595aa6d69f4fd2aa0b425528db9fbc1e
Jack Stinchcombe <sunnysideupdevs@gmail.com>
2016-07-29 13:15:27 +1200

    Initial commit

---
+++ code/forms/fields/CMSEditLinkField.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class CMSEditLinkField extends ReadonlyField
+{
+    protected $linkedObject = "";
+
+    function __construct($name, $title = '', $linkedObject) {
+        $name .= "_CMSEditLink";
+        if($linkedObject && $linkedObject->exists() && $linkedObject->hasMethod("CMSEditLink")) {
+            $this->linkedObject = $linkedObject;
+            $content = '<p><a href="'.$linkedObject->CMSEditLink().'">'.$linkedObject->getTitle().'</a></h3>';
+            $this->dontEscape = true;
+            return parent::__construct($name, $title, $content);
+        }
+        else {
+            return parent::__construct($name, $title);
+        }
+    }
+}
