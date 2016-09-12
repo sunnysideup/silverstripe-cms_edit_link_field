63b411c2587dee3a49942b308492a3b079b7985b
www-data <sunnysideupdevs@gmail.com>
2016-09-12 17:24:39 +1200

    fix ups

--- code/forms/fields/CMSEditLinkField.php
+++ code/forms/fields/CMSEditLinkField.php
@@ -3,17 +3,18 @@
 
 class CMSEditLinkField extends ReadonlyField
 {
-    protected $linkedObject = "";
+    protected $linkedObject = '';
 
-    function __construct($name, $title = '', $linkedObject) {
-        $name .= "_CMSEditLink";
-        if($linkedObject && $linkedObject->exists() && $linkedObject->hasMethod("CMSEditLink")) {
+    public function __construct($name, $title, $linkedObject)
+    {
+        $name .= '_CMSEditLink';
+        if ($linkedObject && $linkedObject->exists() && $linkedObject->hasMethod('CMSEditLink')) {
             $this->linkedObject = $linkedObject;
             $content = '<p><a href="'.$linkedObject->CMSEditLink().'">'.$linkedObject->getTitle().'</a></h3>';
             $this->dontEscape = true;
+
             return parent::__construct($name, $title, $content);
-        }
-        else {
+        } else {
             return parent::__construct($name, $title);
         }
     }
