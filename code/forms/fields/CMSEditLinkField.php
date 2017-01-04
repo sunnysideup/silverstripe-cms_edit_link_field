7bac576087411b17b9609a35cd9439e6407be2e3
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-01-04 15:18:58 +1300

    MAJOR: improved version

--- code/forms/fields/CMSEditLinkField.php
+++ code/forms/fields/CMSEditLinkField.php
@@ -5,12 +5,27 @@ class CMSEditLinkField extends ReadonlyField
 {
     protected $linkedObject = '';
 
-    public function __construct($name, $title, $linkedObject)
+    /**
+     *
+     * @param string $name                 e.g. MyLinkedObjectID
+     * @param string $title                e.g. My Fancy Title
+     * @param DataObject $linkedObject     e.g. $this->MyLinkedObjectID()
+     * @param string $methodOrVariable     (OPTIONAL) - e.g. MyFullTitle
+     */
+    public function __construct($name, $title, $linkedObject, $methodOrVariable = 'getTitle')
     {
         $name .= '_CMSEditLink';
         if ($linkedObject && $linkedObject->exists() && $linkedObject->hasMethod('CMSEditLink')) {
             $this->linkedObject = $linkedObject;
-            $content = '<p><a href="'.$linkedObject->CMSEditLink().'">'.$linkedObject->getTitle().'</a></h3>';
+            if($linkedObject->hasMethod($methodOrVariable)) {
+                $description = $linkedObject->$methodOrVariable();
+            } elseif(isset($linkedObject->$methodOrVariable)) {
+                $description = $linkedObject->$methodOrVariable;
+            } else {
+                $description = 'ERROR!';
+                user_error($methodOrVariable.' does not exist on '.$linkedObject.' (as method or variable)');
+            }
+            $content = '<p><a href="'.$linkedObject->CMSEditLink().'">'.$description.'</a></h3>';
             $this->dontEscape = true;
 
             return parent::__construct($name, $title, $content);
@@ -18,4 +33,5 @@ class CMSEditLinkField extends ReadonlyField
             return parent::__construct($name, $title);
         }
     }
+
 }
