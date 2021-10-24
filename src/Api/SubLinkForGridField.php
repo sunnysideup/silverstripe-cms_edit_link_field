e9f914a1092525ea1278910fee7f1a0ad4b27a10
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-10-25 08:55:39 +1300

    WIP

--- src/Api/SubLinkForGridField.php
+++ src/Api/SubLinkForGridField.php
@@ -6,12 +6,13 @@ use SilverStripe\Control\Controller;
 
 class SubLinkForGridField
 {
-    public static function edit_link(string $fieldName, int $id) : string
+    public static function edit_link(string $fieldName, int $id): string
     {
         $link = $_SERVER['REQUEST_URI'];
         $link = rtrim($link, '/edit');
-        $toAdd = 'ItemEditForm/field/'.$fieldName.'/item/'.$id.'/edit';
+
+        $toAdd = 'ItemEditForm/field/' . $fieldName . '/item/' . $id . '/edit';
+
         return Controller::join_links($link, $toAdd);
     }
-
 }
